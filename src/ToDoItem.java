f75df16bb6165ab9553eefbe6c982d5f61f2ee32
Patrick Gordon <gordonpatrick61@gmail.com>
2026-03-06 14:02:27 +1000

    added ToDoItem class

---
+++ src/ToDoItem.java
@@ -0,0 +1,12 @@
+public class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    ToDoItem()
+    {
+
+    }
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
+}
