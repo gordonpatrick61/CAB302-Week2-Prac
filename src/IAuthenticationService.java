9e476da4658687cd39a5e56eb3bffc6b3bb4af09
WilliamO-creator <williamolufson@icloud.com>
2026-03-06 14:40:11 +1000

    LogIn Method Added

--- src/IAuthenticationService.java
+++ src/IAuthenticationService.java
@@ -29,6 +29,12 @@ public class AuthenticationService implements IAuthenticationService {
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
     public User logIn(String username, String password)
     {
-
+        for (User u : users) {
+            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
+                return u;
+            }
+        }
+        return null; // No match found
+    }
     }
 }
