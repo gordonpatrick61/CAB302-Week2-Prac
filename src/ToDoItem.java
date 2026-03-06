0db345c15d171a4c12f4398a3a3873010454716f
Patrick Gordon <gordonpatrick61@gmail.com>
2026-03-06 14:09:02 +1000

    constructor created

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -3,9 +3,10 @@ public class ToDoItem {
     private boolean isDone;
 
     // TODO Now:
-    ToDoItem()
+    Public ToDoItem(String description)
     {
-
+        isDone = false;
+        description = this.description;
     }
     // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
     // Add getters and setters for each field
